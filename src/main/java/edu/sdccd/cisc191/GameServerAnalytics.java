1f2897a104d60493960b48b0b6d4c65cc4c292c2
EvaSerra <eserrasierra@gmail.com>
2026-05-06 22:31:31 -0700

    Added some java doc to GameServerAnalytics

--- src/main/java/edu/sdccd/cisc191/GameServerAnalytics.java
+++ src/main/java/edu/sdccd/cisc191/GameServerAnalytics.java
@@ -5,6 +5,14 @@ import java.util.stream.Collectors;
 
 public class GameServerAnalytics {
 
+    /**
+     * Returns the usernames of the top N rated players.
+     *
+     * @param players collection with all the players that will be checked
+     * @param n number of players that should be returned
+     * @return List<String> with only the top N players
+     */
+
     public static List<String> findTopNUsernamesByRating(Collection<PlayerAccount> players, int n) {
 
         return players.stream()
@@ -14,6 +22,13 @@ public class GameServerAnalytics {
                 .toList();
     }
 
+    /**
+     * Computes the average rating by each region
+     *
+     * @param players collection with all the players that will be used
+     * @return Map<String, Double> with the region as key and the average rating as value.
+     */
+
     public static Map<String, Double> averageRatingByRegion(Collection<PlayerAccount> players) {
 
         return players.stream()
@@ -22,6 +37,13 @@ public class GameServerAnalytics {
                         Collectors.averagingInt(PlayerAccount::rating)
                 ));
     }
+
+    /**
+     * Find the repeating usernames in a collection.
+     *
+     * @param players collection with all the players that will be used
+     * @return Set<String> with the usernames of every repeated user.
+     */
 
     public static Set<String> findDuplicateUsernames(Collection<PlayerAccount> players) {
 
@@ -36,6 +58,13 @@ public class GameServerAnalytics {
         return duplicates;
     }
 
+    /**
+     * Collects every username by their player tier.
+     *
+     * @param players collection with all the players that will be used
+     * @return Map<String, List<String> with the tier as key and the list of player on that tier as value.
+     */
+
     public static Map<String, List<String>> groupUsernamesByTier(Collection<PlayerAccount> players) {
 
         return players.stream()
@@ -44,6 +73,13 @@ public class GameServerAnalytics {
                         Collectors.mapping(PlayerAccount::username, Collectors.toList())
                 ));
     }
+
+    /**
+     * Groups all the match summaries by the players in the match
+     *
+     * @param matches collection with all the played matches
+     * @return Map<String, List<String> with the username as key and the list of match summaries as value.
+     */
 
     public static Map<String, List<String>> buildRecentMatchSummariesByPlayer(Collection<MatchRecord> matches) {
 
@@ -57,6 +93,15 @@ public class GameServerAnalytics {
         }
         return result;
     }
+
+    /**
+     * Returns the higher value according to a specified comparator
+     *
+     * @param first first value or object
+     * @param second second value or object.
+     * @param comparator comparator that specifies the parameters for the comparison.
+     * @return highest value between the two according to comparator.
+     */
 
     public static <T> T pickHigherRated(T first, T second, Comparator<T> comparator) {
 
